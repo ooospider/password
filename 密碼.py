0bdd219725518578c5fa31fffa0baea7f00c3ff9
Aaron Fan <roma94620@gmail.com>
2021-07-24 12:15:06 +0800

    dfjj

--- 密碼.py
+++ 密碼.py
@@ -1,13 +1,19 @@
 password = 'a123456'
 x = 3
-while True:
+while x > 0 :
+	x = x-1
 	pwd = input ('請輸入您的密碼:')
 	if pwd == password :
 		print ('登入成功')
 		break
 	else :
-		x = x-1
-		print ('還剩下',x,'次機會')
-		if x == 0 :
-			print ('登入失敗!')
-			break
+		print ('密碼錯誤')
+		if x > 0 :
+			print ('還剩下',x,'次機會')
+			     
+
+
+
+
+
+			     
